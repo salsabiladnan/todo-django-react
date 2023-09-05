9676c9189998861d59a53245c505a3225afed000
salsabiladnan <salsabilaadnan11@gmail.com>
2023-09-06 00:01:08 +0700

    connecting react and django

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -55,10 +55,14 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'todo_drf.urls'
 
+import os
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend/build')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend/build/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
