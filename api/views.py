93e49ff67cb3f78403377f962bee3f27041c34c1
salsabiladnan <salsabilaadnan11@gmail.com>
2023-09-05 10:48:43 +0700

    create tasklist

--- api/views.py
+++ api/views.py
@@ -3,7 +3,9 @@ from django.http import JsonResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import TaskSerializer
 
+from .models import Task
 # Create your views here.
 
 @api_view(['GET'])
@@ -17,3 +19,9 @@ def apiOverview(request):
 	}
 
     return Response(api_urls)
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
